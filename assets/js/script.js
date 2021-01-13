ea0d16c1d29a8efda0540d7a7cd1ec2da18a68b9
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-01-13 12:43:55 -0600

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-// this is the timer
 var timeLeft = 75;
-
 var countEl = document.querySelector("#count");
 var startBtn = document.querySelector(".start-btn");
 var h1El = document.querySelector("h1");
@@ -11,8 +9,8 @@ var answerTwo = document.querySelector("#answer-two");
 var answerThree = document.querySelector("#answer-three");
 var answerFour = document.querySelector("#answer-four");
 
+// this is the timer
 function setCounterText() {
-    // var timeLeft = 75;
   
     var timeInterval = setInterval(function() {
       countEl.textContent = timeLeft;
@@ -26,6 +24,7 @@ function setCounterText() {
     }, 1000);
 }
 
+// this is the first question to be asked
 function askQuestionOne(){
   // removes h1
   h1El.outerHTML = "";
@@ -33,13 +32,21 @@ function askQuestionOne(){
   startBtn.outerHTML = "";
   // changes h2 text to first question
   h2El.innerText = "This is question number one";
-  // this adds answer options HTML
+  // this adds answer buttons into the HTML
   answerOne.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-answerOne\">Answer 1</button>";
   answerTwo.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-answerTwo\">Answer 2</button>";
   answerThree.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-answerThree\">Answer 3</button>";
   answerFour.innerHTML = "<button type=\"button\" class=\"btn btn-secondary btn-answerFour\">Answer 4</button>";
+
+  // checks answer
+  if (answerOne.clicked === true){
+    console.log("You're correct");
+  } else {
+    console.log("You're incorrect");
+  }
 }
 
+// when the start quiz button is clicked, the program will run
 startBtn.addEventListener("click", function() {
     setCounterText();
     askQuestionOne();
