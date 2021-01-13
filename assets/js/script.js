14e5315fc871a4e1b770ca4369a1484a9aeb4dd1
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-01-13 09:12:34 -0600

    created question option divs in HTML and inputted them with askQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ var countEl = document.querySelector("#count");
 var startBtn = document.querySelector(".start-btn");
 var h1El = document.querySelector("h1");
 var h2El = document.querySelector("h2");
+var containerEl = document.querySelector("container");
+var answerOne = document.querySelector("#answer-one");
+var answerTwo = document.querySelector("#answer-two");
+var answerThree = document.querySelector("#answer-three");
+var answerFour = document.querySelector("#answer-four");
 
 function setCounterText() {
     // var timeLeft = 75;
@@ -26,10 +31,13 @@ function askQuestionOne(){
   h1El.outerHTML = "";
   // removes button
   startBtn.outerHTML = "";
-  // changes h2 to first question
-  h2El.innerHTML = "This is question number one";
-
-
+  // changes h2 text to first question
+  h2El.innerText = "This is question number one";
+  // this adds answer options HTML
+  answerOne.innerHTML = "<h3>This is a question option1</h3>"
+  answerTwo.innerHTML = "<h3>This is a question option2</h3>"
+  answerThree.innerHTML = "<h3>This is a question option3</h3>"
+  answerFour.innerHTML = "<h3>This is a question option4</h3>"
 }
 
 startBtn.addEventListener("click", function() {
